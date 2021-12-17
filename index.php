064213cc256cb8b0fc1fc1f56663abc74f89d702
cag185 <cag185@pitt.edu>
2021-12-17 11:53:20 -0500

    Attempting to get UPDATE to work to update the TIME_LEFT variable in the databse

--- index.php
+++ index.php
@@ -39,30 +39,72 @@
 		{
 			die("Connection failed: " . $conn->connect_error);
 		}
+		else{
+			echo "Connection Successful -- Data accessable <br>";
+		}
 		//Cant seem to load date assigned
-		//$sql = "SELECT CHORE, NAME_ASSIGNED, _STATUS, DATE_ASSIGNED, POINTS, INDEX_ FROM ChoreData";
-		$sql = "SELECT CHORE, NAME_ASSIGNED, _STATUS, POINTS, INDEX_ FROM ChoreData";
+
+		//SQL command to select what data I want to use
+		$sql = "SELECT CHORE, NAME_ASSIGNED, _STATUS, CURRENT_TIMESTAMP, POINTS, TIME_LEFT, DUE_DATE, INDEX_ FROM ChoreData";
+		//Assigning a value to hold the queried data
+		$result = $link->query($sql);
+		//using a value to access the files in the result variable
+		$res = $result->fetch_assoc();
+		//$currTime = TIMESTAMPDIFF(HOUR, CURRENT_TIMESTAMP, DUE_DATE);
+		$currTime = $res["CURRENT_TIMESTAMP"];
+		$dueDate = $res["DUE_DATE"];
+		//$res = $link->query($sql);
+		echo strtotime($currTime);
+		echo "<br>";
+		echo strtotime($dueDate);
+
+		//----TimeDiff function
+		function timeDiff($firstTime, $secondTime)
+		{
+			//convert to unix timestamps
+			$firstTime = strtotime($firstTime);
+			$secondTime = strtotime($secondTime);
+
+			//subtract
+			if($secondTime > $firstTime)
+				$interval1 = $secondTime - $firstTime;
+			else
+				$interval1 = $firstTime - $secondTime;
+			//format in Hours Minutes Days
+			$interval1 = date("h:i:s", $interval1);
+			//return 
+			return $interval1;
+		}
+
+		//create a variable to hold the difference
+		$diffe = timeDiff($dueDate, $currTime);
+		//$interval = $currTime->diff($dueDate);
+		echo "<br> New Time Difference: ";
+		echo " <br> stuff should be here <br>";
+		echo $diffe;
+		//echo $interval->date_format($interval, %H %i %s");
+
+		//new SQL command to update the DataBase with a Time left
+		$sql = "UPDATE ChoreData SET TIME_LEFT = " . $diffe;
+		//$sql = "UPDATE ChoreData SET TIME_LEFT  = CURRENT_TIME";
+
+		$result = $link->query($sql);	//actually creates query to update in DB
+		//now that TIME_LEFT is updated, create a SQL command to select relevant data, create query, store query in variable
+		$sql = "SELECT CHORE, NAME_ASSIGNED, _STATUS, CURRENT_TIMESTAMP, POINTS, TIME_LEFT, DUE_DATE, INDEX_ FROM ChoreData";
+		//Assigning a value to hold the queried data
 		$result = $link->query($sql);
 
-		//if($result->num_rows > -1)
-		{
-		echo "<table> <tr> <th>Chore</th> <th>Name Assigned</th> <th>Status</th> <th>Due Date</th> <th>Points</th> <th>Index</th> </tr>";
-		//echo "<table><tr><th>Chore</th><th>Name Assigned</th></tr>";
+		//$sql = "SELECT DUE_DATE, CURRENT_TIME, TIME_LEFT, TIMESTAMPDIFF(HOUR, DUE_DATE, CURRENT_TIME) AS TIME_LEFT FROM ChoreData";
+		
+		echo "<table> <tr> <th>Chore</th> <th>Name Assigned</th> <th>Status</th> <th>Due Date</th> <th>Time Remaining</th> <th>Points</th> <th>Index</th> </tr>";
 		//output the data of each row
-		while ($row = $result->fetch_assoc()) {
-			echo "<tr><td>".$row["CHORE"]."</td><td>".$row["NAME_ASSIGNED"]."</td><td>".$row["_STATUS"]."</td><td>".$row["POINTS"]."</td><td>".$row["INDEX_"]."</td></tr>";
+		while ($row = $result->fetch_assoc() ) {
+			echo "<tr><td>".$row["CHORE"]."</td><td>".$row["NAME_ASSIGNED"]."</td><td>".$row["_STATUS"]."</td><td>".$row["DUE_DATE"]."</td><td>".$row["TIME_LEFT"]."</td><td>".$row["POINTS"]."</td><td>".$row["INDEX_"]."</td></tr>";
 			//echo "<tr><td>".$row["CHORE"]."</td><td>".$row["NAME_ASSIGNED"]."</td></tr>";
 		}
 		echo "</table>";
-		}
-		//else
-		{
-		//echo "0 results";
-		}
-
 
 		$link->close();
-
 		?>
 	</body>
 </HTML> 
